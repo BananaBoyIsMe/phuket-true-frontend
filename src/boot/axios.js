cec95c5eea78a22c81aa7c0db8bad7b01b17a4c6
dog <no>
2023-12-01 13:17:49 +0700

    first commit

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,8 +8,10 @@ import axios from "axios";
 // "export default () => {}" function below (which runs individually
 // for each client)
 // const RESTAPI = "https://phuket-web-backend.vercel.app/api";
+// const RESTAPI = "https://phuket-true-backend.vercel.app/api/";
 //const RESTAPI = "https://manage-account-api.vercel.app/api";
 const RESTAPI = "http://localhost:3000/api/";
+
 const api = axios.create({
   baseURL: RESTAPI,
   headers: { "Content-Type": "application/json" },
